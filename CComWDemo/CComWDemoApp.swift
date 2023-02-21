0e06f216da43633ddc1e0eb5daf7f0c4cac4ab44
Matthew Collins <m.collins@etoncollege.org.uk>
2023-02-21 09:48:52 +0000

    Initial Commit

---
+++ CComWDemo/CComWDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CComWDemoApp.swift
+//  CComWDemo
+//
+//  Created by Collins, Matthew - MC on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CComWDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
